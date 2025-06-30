4e89c4f7960ccf855522dc8085c8bee17749cbbd
joaopedro760 <joao.de.ferreira02@escola.pr.gov.br>
2025-06-30 10:46:02 -0300

    Create perguntas.js

---
+++ perguntas.js
@@ -0,0 +1,41 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
+
+criaCartao(
+    'Lingua Portuguesa',
+    'O que é verbo?',
+    'um verbo é uma palavra que expressa ação, estado, mudança de estado ou fenômeno da natureza'
+)
+
+criaCartao(
+  'História',
+  'Em qual cidade começou a Primeira Guerra Mundial?',
+  'Sarajevo'
+)
+
+criaCartao(
+  'ciências',
+  'Qual é a diferença entre matéria escura e energia escura no universo?',
+  ' A matéria escura exerce uma força gravitacional que atrai a matéria visível, ajudando a formar e manter a estrutura do universo, como galáxias e aglomerados de galáxias.'
+)
